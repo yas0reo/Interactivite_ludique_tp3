dc6ff09595874896d8e41cae9deb31767c9bf1e6
yasmine <yass.onekeo@gmail.com>
2025-11-04 17:43:58 -0500

    base_niveau01

--- plateforme-niveaux/scripts/menu.gd
+++ plateforme-niveaux/scripts/menu.gd
@@ -3,10 +3,12 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/niveau_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/niveau_0.tscn")
 
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/optionsMenuScene.tscn")
 
+
+func _on_quitter_pressed() -> void:
 	get_tree().quit()
